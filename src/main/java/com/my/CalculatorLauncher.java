ded53480869c00f1ce99d825dc0c91c569528296
swisterska <278448@student.pwr.edu.pl>
2025-10-16 12:05:35 +0200

    Add substraction

--- src/main/java/com/my/CalculatorLauncher.java
+++ src/main/java/com/my/CalculatorLauncher.java
@@ -4,4 +4,8 @@ public class CalculatorLauncher {
     public static void main(String[] args) {
         System.out.println("Coming soon!");
     }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
 }
